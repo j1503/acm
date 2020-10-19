f2c036f62bea203ddf248e4bd26eeebceb06bcf8
Julian B <j1503@users.noreply.github.com>
2020-10-19 21:52:28 +0200

    - added many new functions such as some of the SDL functions - added getFunctionPointerByRelativeCall to simplify getting a function pointer from a relative call

--- memory.cpp
+++ memory.cpp
@@ -1,8 +1,24 @@
 #include "memory.hpp"
-#include <Windows.h>
-#include <Psapi.h>
 
-uintptr_t memory::findPatternNaiive(const char* moduleName, const char* pattern) noexcept
+#include <gl/GL.h>
+#include <type_traits>
+#pragma comment(lib,"opengl32.lib")
+#include "helper.hpp"
+
+inline static bool patternSearchMemcmp(const unsigned char* word, const unsigned char* text, size_t size) {
+	while (size) {
+		if (*word != '?') {
+			if (*word != *text) return false;
+		}
+		--size;
+		++word;
+		++text;
+	}
+	return true;
+};
+
+template <class T, size_t OFFSET = 0u>
+static T findPatternNaiive(const char* moduleName, const char* pattern) noexcept
 {
 	HMODULE hModule = GetModuleHandle(moduleName);
 	if (hModule) {
@@ -26,30 +42,19 @@ uintptr_t memory::findPatternNaiive(const char* moduleName, const char* pattern)
 			}
 
 			if (!*second)
-				return reinterpret_cast<uintptr_t>(start);
+				return reinterpret_cast<T>(start + OFFSET);
 		}
 	}
 	return 0;
 }
 
 // ~7.5x faster than findPatternNaiive
-uintptr_t memory::findPatternBMH(const char* moduleName, const char* pattern) noexcept
-{
+template <class T, size_t OFFSET = 0u>
+static T findPatternBMH(const char* moduleName, const char* pattern) noexcept {
 	HMODULE hModule = GetModuleHandle(moduleName);
 	if (hModule) {
 		MODULEINFO moduleInfo;
 		if (GetModuleInformation(GetCurrentProcess(), hModule, &moduleInfo, sizeof(moduleInfo))) {
-			auto patternSearchMemcmp = [](const unsigned char* word, const unsigned char* text, size_t size) -> bool {
-				while (size) {
-					if (*word != '?') {
-						if (*word != *text) return false;
-					}
-					--size;
-					++word;
-					++text;
-				}
-				return true;
-			};
 			const unsigned char* text = (const unsigned char*)moduleInfo.lpBaseOfDll;
 			const unsigned char* word = (const unsigned char*)pattern;
 
@@ -69,33 +74,22 @@ uintptr_t memory::findPatternBMH(const char* moduleName, const char* pattern) no
 			while (textPos <= (textLength - wordLength)) {
 				const unsigned char curr = text[textPos + maxWordIdx];
 				if (((curr == lastWordChar) || lastWordChar == '?') && patternSearchMemcmp(word, text + textPos, maxWordIdx)) {
-					return (uintptr_t)text + textPos;
+					return (T)(text + textPos + OFFSET);
 				}
 				textPos += table[curr];
 			}
 		}
 	}
-	return 0;
+	return (T)0;
 }
 
-bool memory::isPatternUnique(const char* moduleName, const char* pattern) noexcept
+static bool isPatternUnique(const char* moduleName, const char* pattern) noexcept
 {
 	size_t occ = 0;
 	HMODULE hModule = GetModuleHandle(moduleName);
 	if (hModule) {
 		MODULEINFO moduleInfo;
 		if (GetModuleInformation(GetCurrentProcess(), hModule, &moduleInfo, sizeof(moduleInfo))) {
-			auto patternSearchMemcmp = [](const unsigned char* word, const unsigned char* text, size_t size) -> bool {
-				while (size) {
-					if (*word != '?') {
-						if (*word != *text) return false;
-					}
-					--size;
-					++word;
-					++text;
-				}
-				return true;
-			};
 			const unsigned char* text = (const unsigned char*)moduleInfo.lpBaseOfDll;
 			const unsigned char* word = (const unsigned char*)pattern;
 
@@ -124,14 +118,113 @@ bool memory::isPatternUnique(const char* moduleName, const char* pattern) noexce
 	return occ == 1;
 }
 
-memory::memoryManager::memoryManager()
+template <class FunctionPointerType, size_t OPCODE_SIZE = 1u> // opcode size in bytes (e.g. 1u for "call") 
+// requires Pointer<FunctionPointerType>
+inline static FunctionPointerType getAbsoluteAddress(void* EIP)
 {
-	this->localPlayer = **(playerent***)(memory::findPatternBMH("ac_client.exe", "\x8B\x35????\x83\xC4\x04\xE8????\x8B\xCD") + 2);
-	this->entityList = **(playerent****)(memory::findPatternBMH("ac_client.exe", "\x8B\x0D????\x8B\x34\x91\x85\xF6\x0F\x84????") + 2);
-	this->entityCount = *(size_t**)(memory::findPatternBMH("ac_client.exe", "\x39\x15????\x89\x54\x24\x14") + 2);
-	this->viewMatrix = (glm::mat4*)(memory::findPatternBMH("ac_client.exe", "\xE8????\x38\x5C\x24\x3C") + 1);
+	static_assert(std::is_pointer<FunctionPointerType>::value, "violation of requirement Pointer<FunctionPointerType>");
+	constexpr size_t JMP_LEN = 5u;
+	return (FunctionPointerType)(*(uintptr_t*)(uintptr_t(EIP) + OPCODE_SIZE) + JMP_LEN + uintptr_t(EIP));
 }
 
-memory::memoryManager::~memoryManager()
+// FunctionPointerType = function pointer type ( e.g. int(__cdecl*)(int) )
+// OFFSET = offset to call instruction from address returned by findPattern() (0 most of the time)
+// OPCODE_SIZE = opcode size in bytes (e.g. 1u for "call") 
+template <class FunctionPointerType, size_t OFFSET = 0u, size_t OPCODE_SIZE = 1u>
+// requires Pointer<FunctionPointerType>
+static FunctionPointerType getFunctionPointerByRelativeCall(const char * moduleName, const char * patternToCall) {
+	static_assert(std::is_pointer<FunctionPointerType>::value, "violation of requirement Pointer<FunctionPointerType>");
+	auto relative = findPatternBMH<void*, OFFSET>(moduleName, patternToCall);
+	if (!relative) return nullptr;
+	return getAbsoluteAddress<FunctionPointerType, OPCODE_SIZE>(relative);
+}
+
+
+memory::memoryManager::memoryManager()
 {
+	this->localPlayer = **findPatternBMH<playerent***, 2>("ac_client.exe", "\x8B\x35????\x83\xC4\x04\xE8????\x8B\xCD");
+	this->entityList = *findPatternBMH<playerent****, 2>("ac_client.exe", "\x8B\x0D????\x8B\x34\x91\x85\xF6\x0F\x84????");
+	this->entityCount = *findPatternBMH<size_t**, 2>("ac_client.exe", "\x39\x15????\x89\x54\x24\x14");
+	this->viewMatrix = *findPatternBMH<float***, 1>("ac_client.exe", "\xB8????\xB9????\x5E\x83\xC4\x10");
+	this->lookingAt = *findPatternBMH<vec**, 1>("ac_client.exe", "\xA3????\xD8\xC9\xD9\x5C\x24\x08\x8B\x4C\x24\x08");
+	this->screenWidth = *findPatternBMH<size_t**,2>("ac_client.exe", "\x8B\x0D????\x83\xEC\x38\x56\x57\x50\x51");
+	this->screenHeight = *findPatternBMH<size_t**, 1>("ac_client.exe", "\xA1????\x8B\x0D????\x83\xEC\x38");
+
+	this->SDL_WM_GrabInput = helper::getExportedCdeclFunction<SDL_GrabMode, SDL_GrabMode>("SDL.dll", "SDL_WM_GrabInput");
+	this->SDL_ShowCursor = helper::getExportedCdeclFunction<int, int>("SDL.dll", "SDL_ShowCursor");
+	this->SDL_PollEvent = helper::getExportedCdeclFunction<int, SDL_Event*>("SDL.dll", "SDL_PollEvent");
+	this->SDL_PushEvent = helper::getExportedCdeclFunction<int, SDL_Event*>("SDL.dll", "SDL_PushEvent");
+	this->SDL_EventState = helper::getExportedCdeclFunction<Uint8, Uint32, int>("SDL.dll", "SDL_EventState");
+	this->SDL_PeepEvents = helper::getExportedCdeclFunction<int, SDL_Event*, int, SDL_eventaction, Uint32, Uint32>("SDL.dll", "SDL_PeepEvents");
+
+	// FIX!
+	this->executeRet = getFunctionPointerByRelativeCall<int(__cdecl*)(const char*)>("ac_client.exe","\xE8????\x83\xC4\x04\x85\xC0\x74\x04\x89\x44\x24\x54");
+	this->rayIntersectEnt = getFunctionPointerByRelativeCall<void*>("ac_client.exe", "\xE8????\x83\xC4\x14\x89\x03");
+	this->isVisible = getFunctionPointerByRelativeCall<void*>("ac_client.exe", "\xE8????\x83\xC4\x18\x84\xC0\x74\x0C\xB8????");
+	this->hudDrawString = getFunctionPointerByRelativeCall<void*, 5>("ac_client.exe", "\xB8????\xE8????\x83\xC4\x1C\x55\x8D\x54\x24\x3C");
+
+	//auto relativeCallInstructionAddr = findPatternBMH<void*>("ac_client.exe", "\xE8????\x83\xC4\x14\x89\x03");
+	//this->rayIntersectEnt = getAbsoluteAddress<void*>(relativeCallInstructionAddr);
+
+	//relativeCallInstructionAddr = findPatternBMH<void*>("ac_client.exe", "\xE8????\x83\xC4\x18\x84\xC0\x74\x0C\xB8????");
+	//this->isVisible = getAbsoluteAddress<uintptr_t>(relativeCallInstructionAddr);
+
+	//relativeCallInstructionAddr = findPatternBMH<void*,5>("ac_client.exe", "\xB8????\xE8????\x83\xC4\x1C\x55\x8D\x54\x24\x3C");
+	//this->hudDrawString = getAbsoluteAddress<uintptr_t>(relativeCallInstructionAddr);
+}
+
+playerent* memory::memoryManager::callRayIntersectEnt(float* distance, vec* from, vec* to, playerent* ent, int* bone)
+{
+	auto addr = this->rayIntersectEnt;
+	__asm { // custom calling convention
+		push 1; unk
+		push bone
+		push ent
+		push to
+		push from
+		mov edi, distance
+		call addr
+		add esp, 0x14
+	}
+	// returns eax
+}
+
+bool memory::memoryManager::callIsVisible(const vec from, const vec to)
+{
+	auto addr = this->isVisible;
+	__asm {
+		push to.z
+		push to.y
+		push to.x
+		push from.z
+		push from.y
+		push from.x
+
+		xor cl,cl
+		xor eax, eax
+
+		call addr
+		add esp, 0x18
+		// result in al
+	}
+}
+
+void memory::memoryManager::callHudDrawString(unsigned char color[3], const char* text, int x, int y)
+{
+	unsigned char red = color[0];
+	unsigned char green = color[1];
+	unsigned char blue = color[2];
+	auto addr = this->hudDrawString;
+	__asm {
+		push -1
+		push -1
+		push blue
+		push red
+		push y
+		push x
+		push text
+		mov al, green
+		call addr
+		add esp, 0x1C
+	}
 }
--- memory.hpp
+++ memory.hpp
@@ -3,7 +3,11 @@
 #pragma once
 
 #include <memory>
-#include <glm/mat4x4.hpp>
+#include <SDL.h>
+#define NOMINMAX
+#include <Windows.h>
+#include <Psapi.h>
+#include "sdk/geom.hpp"
 #include "sdk/entity.hpp"
 
 namespace memory
@@ -11,15 +15,33 @@ namespace memory
 	class memoryManager {
 	public:
 		memoryManager();
-		~memoryManager();
+		~memoryManager() = default;
+		// pointers
 		playerent* localPlayer;
-		playerent** entityList;
+		playerent*** entityList;
 		size_t* entityCount;
-		glm::mat4* viewMatrix;
+		float** viewMatrix;
+		vec* lookingAt;
+		size_t* screenWidth;
+		size_t* screenHeight;
+		// function pointers
+		// sdl
+		SDL_GrabMode(__cdecl* SDL_WM_GrabInput)(SDL_GrabMode);
+		int(__cdecl* SDL_ShowCursor)(int toggle);
+		Uint8(__cdecl* SDL_EventState)(Uint32 type, int state);
+		int(__cdecl*SDL_PollEvent)(SDL_Event* event);
+		int(__cdecl* SDL_PushEvent)(SDL_Event* event);
+		int(__cdecl* SDL_PeepEvents)(SDL_Event* events, int numevents, SDL_eventaction action, Uint32 mintype, Uint32 maxType);
+		// other
+		int(__cdecl* executeRet)(const char* p);
+		void* rayIntersectEnt; // __usercall (ukn1@<edi>) -> cant use function pointer
+		void* isVisible;
+		void* hudDrawString; // __usercall (GLubyte green@<al>) 
+		// extra (helper) functions
+		playerent* callRayIntersectEnt(float* distance, vec* from, vec* to, playerent* ent, int* bone);
+		bool callIsVisible(const vec from, const vec to);
+		void callHudDrawString(unsigned char color[3], const char* text, int x, int y);
 	};
-	uintptr_t findPatternNaiive(const char* moduleName, const char* pattern) noexcept;
-	uintptr_t findPatternBMH(const char* moduleName, const char* pattern) noexcept;
-	bool isPatternUnique(const char* moduleName, const char* pattern) noexcept;
 }
 
 namespace globals {
