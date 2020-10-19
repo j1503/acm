fd3f09689bd90030b8330caba854599af07c1fbf
Julian B <j1503@users.noreply.github.com>
2020-10-19 21:53:35 +0200

    - moved special input from wndproc to hkSDL_GL_SwapBuffers - changed wglSwapbuffers hook to SDL_GL_SwapBuffers

--- hookmanager.cpp
+++ hookmanager.cpp
@@ -1,17 +1,30 @@
 #include "hookmanager.hpp"
 #include "hookedfunctions.hpp"
+
+#include "gui/imgui/imgui.h"
 #include "helper.hpp"
+#include "debugconsole.hpp"
+#include "input.hpp"
+#include "memory.hpp"
+#include "config.hpp"
+#include "cheats/cheatmanager.hpp"
+#include "gui/gui.hpp"
 
 static LRESULT __stdcall wndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 static bool __stdcall unload(HMODULE hModule);
+extern IMGUI_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 static LRESULT __stdcall wndProcThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	SetWindowLongPtr(hWnd, GWLP_WNDPROC, (LONG_PTR)wndProc);
-
-	// init stuff here
+#ifdef _DEBUG
+	globals::DebugConsole = std::make_unique<debug::console>();
+#endif
+	globals::ConfigManager = std::make_unique<configManager>(hWnd);
 	globals::MemoryManager = std::make_unique<memory::memoryManager>();
-	globals::ConfigManager = std::make_unique<configManager>();
+	globals::InputManager = std::make_unique<input>();
+	globals::CheatManager = std::make_unique<cheats::cheatManager>();
+	globals::GUIManager = std::make_unique<drawing::gui>(hWnd);
 
 	globals::HookManager->install();
 
@@ -20,38 +33,64 @@ static LRESULT __stdcall wndProcThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM
 
 static LRESULT __stdcall wndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	// imgui
+	if (globals::ActiveProfile->general.active && ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam)) {
+		return 1l;
+	}
+	switch (msg) {
+	case WM_QUIT:
+		globals::HookManager->uninstall(); // cleanup, even on program termination
+		break;
+	//case WM_KEYDOWN:
+	//case WM_SYSKEYDOWN:
+	//	if (wParam == globals::ActiveProfile->general.ejectKey) {
+	//		globals::HookManager->uninstall();
+	//		return 0l;
+	//	}
+	//	else if (wParam == globals::ActiveProfile->general.menuKey) {
+	//		globals::GUIManager->toggle();
+	//	}
+	//	break;
+	}
+
 	return CallWindowProc(globals::HookManager->originalWndProc, hWnd, msg, wParam, lParam);
 }
 
 hooks::hookManager::hookManager(HMODULE hModule)
-	: hModule(hModule)
+	: hModule(hModule), sdlswapbuffers(), pollevents()
 {
-	this->hWnd = FindWindow(0, "AssaultCube");
+	this->hWnd = FindWindow(nullptr, "AssaultCube");
+
 	this->originalWndProc = WNDPROC(SetWindowLongPtr(this->hWnd, GWLP_WNDPROC, (LONG_PTR)wndProcThunk));
 }
 
 void hooks::hookManager::install()
 {
 	// create hooks
-	this->swapbuffers = hooks::trampoline("opengl32.dll", "wglSwapBuffers", hookedfunctions::hkwglSwapBuffers, 0x5);
-
+	//this->swapbuffers = hooks::trampoline("opengl32.dll", "wglSwapBuffers", hookedfunctions::hkwglSwapBuffers, 0x5);
+	this->pollevents = hooks::trampoline("SDL.dll", "SDL_PollEvent", hookedfunctions::hkSDL_PollEvent, 0x6);
+	this->sdlswapbuffers = hooks::iat("ac_client.exe");
+	
 	// hook
-	this->swapbuffers.hook();
-
-	// set pointers
-	this->wglSwapBuffersGateway = reinterpret_cast<bool(__stdcall*)(HDC)>(this->swapbuffers.getGateway());
+	this->sdlswapbuffers.hook("SDL.dll", "SDL_GL_SwapBuffers", hookedfunctions::hkSDL_GL_SwapBuffers);
+	//this->swapbuffers.hook();
+	this->pollevents.hook();
 }
 
 void hooks::hookManager::uninstall()
 {
-	this->swapbuffers.restore();
+	// restore hooks
+	//this->swapbuffers.restore();
+	this->pollevents.restore();
+	this->sdlswapbuffers.restore("SDL.dll", "SDL_GL_SwapBuffers");
 
+	// restore wndproc & unload
 	SetWindowLongPtr(this->hWnd, GWLP_WNDPROC, (LONG_PTR)this->originalWndProc);
 	HANDLE hThread = CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)unload, this->hModule, 0, nullptr);
 	if (hThread) CloseHandle(hThread);
 }
 
 static bool __stdcall unload(HMODULE hModule) {
-	Sleep(150);
+	Sleep(150); // 
 	FreeLibraryAndExitThread(hModule, 0);
 }
--- hookmanager.hpp
+++ hookmanager.hpp
@@ -4,30 +4,31 @@
 
 #include <Windows.h>
 #include <memory>
-
-#include "memory.hpp"
-#include "config.hpp"
+#include <SDL.h>
 
 #include "hooks/vmt.hpp"
 #include "hooks/bytepatch.hpp"
+#include "hooks/ieat.hpp"
 
 namespace hooks
 {
 	class hookManager
 	{
 	public:
+		hookManager() = default;
 		hookManager(HMODULE hModule);
 		void install();
 		void uninstall();
 		// original function ptrs
-		bool(__stdcall* wglSwapBuffersGateway)(HDC hdC);
 		WNDPROC originalWndProc;
 
 		// other attributes
 		HWND hWnd;
 		HMODULE hModule;
 		//hooks
-		hooks::trampoline swapbuffers;
+		//hooks::trampoline swapbuffers;
+		hooks::trampoline pollevents;
+		hooks::iat sdlswapbuffers;
 	};
 }
 // global
